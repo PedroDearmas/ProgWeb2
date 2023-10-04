e495c9a1039967606b4c6c1a7ecce6570e84b651
Pedro Araujo Dearmas <115725683+PedroDearmas@users.noreply.github.com>
2023-10-03 22:23:05 -0300

    aula 03/10

---
+++ CadastroVendasPagamentoDigital/src/main/java/controllers/ListarVendaServlet.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.VendaPagDigital;
+import models.VendaPagDigitalDAO;
+
+/**
+ * Servlet implementation class ListarVendaServlet
+ */
+@WebServlet("/ListarVendaServlet")
+public class ListarVendaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List<VendaPagDigital> listaVendasPagDigital = new ArrayList<VendaPagDigital>();
+
+		VendaPagDigitalDAO vendaPagDigitalDAO = new VendaPagDigitalDAO();
+
+		
+		listaVendasPagDigital = VendaPagDigitalDAO.listarTodas();
+			
+		
+		request.setAttribute("vendas", listaVendasPagDigital);
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher("ListaDeVendas.jsp");
+		rd.forward(request, response);
+		
+			
+	}
+}
